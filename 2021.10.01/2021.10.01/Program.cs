74c7e94f84407374880d37d35d4b03bf71e5ade1
IbrahimAdam43 <91724145+IbrahimAdam43@users.noreply.github.com>
2021-10-01 11:35:42 +0200

    Házi feladat lesz 4,5,7,8

--- 2021.10.01/2021.10.01/Program.cs
+++ 2021.10.01/2021.10.01/Program.cs
@@ -10,8 +10,51 @@ namespace _2021._10._01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
-            Console.ReadLina();
+            // feladat();
+            //feladat2();
+            //feladat3();
+            // feladat6();
+            feladat9();
+            Console.WriteLine("Enter");
+            Console.ReadLine();
+        }
+        static void feladat()
+        {
+            Console.WriteLine("Hello world!");
+        }
+        static void feladat2()
+        {
+            Console.Write("Kérema neved! ");
+            string nev = Console.ReadLine();
+            Console.WriteLine("Szia {0}!", nev);
+        }
+        static void feladat3()
+        {
+
+        }
+        static void feladat6()
+        {
+            Console.Write("Kérem a négyzet oldalát! a=");
+            double a = double.Parse(Console.ReadLine());
+            double k = 4 * a;
+            Console.WriteLine("A négyzet kerülete {0}",k);
+            double t = a * a;
+            Console.WriteLine("A négyzet területe {0}", t);
+            t = Math.Pow(a,2);
+            Console.WriteLine("A négyzet területe {0} ", t);
+            
+
+        }
+        static void feladat9()
+        {
+            Console.Write("a= ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b= ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("c= ");
+            int c = int.Parse(Console.ReadLine());
+            int legkisebb = Math.Min(a, Math.Min(b, c));
+            Console.WriteLine("A három szám közül a legkisebb{0}", legkisebb);
         }
     }
 }
