fd2be13161d92c11c484e0250b300777f98cdad2
IbrahimAdam43 <91724145+IbrahimAdam43@users.noreply.github.com>
2021-10-05 10:52:31 +0200

    Órai munka

---
+++ 20211005_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211005_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+  
+
+        {
+           
+
+            Console.Write("Kérek egy számot:");
+            int sec = Convert.ToInt32(Console.ReadLine());
+            if (sec % 2 == 0)
+            {
+                Console.WriteLine("Páros a szám");      
+            }
+            else
+            {
+                Console.WriteLine("Páratlan");
+            }
+            Console.Read();
+            
+
+
+        }
+    }
+}
