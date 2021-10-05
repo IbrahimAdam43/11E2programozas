c0d6404b60fa4949753f0f8174ddd926f88fe91a
IbrahimAdam43 <91724145+IbrahimAdam43@users.noreply.github.com>
2021-10-05 11:33:48 +0200

    Elágazások

---
+++ 2021.10.05 (feladat 2) (elágazások)/20211005_2 (feladat 2)/20211005_2 (feladat 2)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211005_2__feladat_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            feladat3();
+            //feladat4();
+        }
+        static void feladat3()
+        {
+            Console.Write("Kérek egy számot: ");
+            int jelek = Convert.ToInt32(Console.ReadLine());
+            if (jelek > 0)
+            {
+                Console.Write("Pozitív");
+            }
+            else
+            {
+                Console.Write("Negatív");
+            }
+            Console.Read();
+        }
+        static void feladat4()
+        {
+        
+        }
+    }
+}
