2dc3e46cace7101ee137063905d1a764d4cb337e
IbrahimAdam43 <91724145+IbrahimAdam43@users.noreply.github.com>
2021-10-14 10:34:10 +0200

    2021.10.14

---
+++ 2021.10.14/2021.10.14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2021._10._14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            feladat2();
+            Console.Read();
+        }
+        static void feladat2()
+        {
+            int sec = int.Parse(Console.ReadLine());
+            switch ()
+            {
+                case 
+                default:
+                    break;
+            }
+        }
+    }
+}
